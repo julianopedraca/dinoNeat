fd87e5cd840b764f87819a5f284ed1a79f54fbc5
julianopedraca <juliano.pedraca@gmail.com>
2022-05-12 20:07:25 -0300

    joginho pronto e com ia

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 import os
 import random
 import sys
+import math
+import neat
 
 pygame.init()
 
@@ -45,6 +47,7 @@ class Dinosaur:
         self.jump_vel = self.JUMP_VEL
         self.rect = pygame.Rect(
             self.X_POS, self.Y_POS, img.get_width(), img.get_height())
+        self.color = (random.randint (0, 255), random.randint(0, 255), random.randint(0, 255))
         self.step_index = 0
 
     def update(self):
@@ -73,6 +76,9 @@ class Dinosaur:
 
     def draw(self, SCREEN):
         SCREEN.blit(self.image, (self.rect.x, self.rect.y))
+        pygame.draw.rect(SCREEN, self.color, (self.rect.x, self.rect.y, self.rect.width, self.rect.height), 2)
+        for obstacle in obstacles:
+            pygame.draw.line(SCREEN, self.color, (self.rect.x + 54, self.rect.y + 12), obstacle.rect.center, 2)
 
 
 class Obstacle:
@@ -105,19 +111,35 @@ class Largecactus (Obstacle):
 
 def remove(index):
     dinosaurs.pop(index)
+    ge.pop(index)
+    nets.pop(index)
+
+def distance(pos_a, pos_b):
+    dx = pos_a[0]-pos_b[0]
+    dy = pos_a[1]-pos_b[1]
+    return math.sqrt(dx**2+dy**2)
 
 
-def main():
-    global game_speed, x_pos_bg, y_pos_bg, obstacles, dinosaurs, points
+def eval_genomes(genomes, config):
+    global game_speed, x_pos_bg, y_pos_bg, obstacles, nets, ge, dinosaurs, points
     clock = pygame.time.Clock()
     points = 0
 
     obstacles = []
-    dinosaurs = [Dinosaur()]
+    dinosaurs = []
+    ge = []
+    nets = []
 
     x_pos_bg = 0
     y_pos_bg = 380
     game_speed = 20
+
+    for genome_id, genome in genomes:
+        dinosaurs.append(Dinosaur())
+        ge.append(genome)
+        net = neat.nn.FeedForwardNetwork.create(genome, config)
+        nets.append(net)
+        genome.fitness = 0
 
     def score():
         global points, game_speed
@@ -126,6 +148,16 @@ def main():
             game_speed += 1
         text = FONT.render(f'points: {str(points)}', True, (0, 0, 0))
         SCREEN.blit(text, (950, 50))
+
+    def statistics():
+        global dinosaur, game_speed, ge
+        text_1 = FONT.render(f'Dinosaurs Alive: {str(len(dinosaurs))}', True, (0, 0, 0))
+        text_2 = FONT.render(f'Generation: {pop.generation+1}', True, (0, 0, 0))
+        text_3 = FONT.render(f'Game Speed: {str(game_speed)}', True, (0, 0, 0))
+
+        SCREEN.blit(text_1, (50, 450))
+        SCREEN.blit(text_2, (50, 480))
+        SCREEN.blit(text_3, (50, 510))
 
     def background():
         global x_pos_bg, y_pos_bg
@@ -166,19 +198,47 @@ def main():
             obstacle.update()
             for i, dinosaur in enumerate(dinosaurs):
                 if dinosaur.rect.colliderect(obstacle.rect):
+                    ge[i].fitness -= 1
                     remove(i)
 
         user_input = pygame.key.get_pressed()
 
         for i, dinosaur in enumerate(dinosaurs):
-            if user_input[pygame.K_SPACE]:
+            output = nets[i].activate((dinosaur.rect.y, distance(
+                (dinosaur.rect.x, dinosaur.rect.y), obstacle.rect.midtop)))
+
+            if output [0] > 0.5 and dinosaur.rect.y == dinosaur.Y_POS:
                 dinosaur.dino_jump = True
                 dinosaur.dino_run = False
 
+            # if user_input[pygame.K_SPACE]:  // use in case you want to play the game
+                # dinosaur.dino_jump = True
+                # dinosaur.dino_run = False
+
+        statistics()
         score()
         background()
         clock.tick(30)
         pygame.display.update()
 
+# Setup the NEAT
 
-main()
+
+def run(config_path):
+    global pop
+    config = neat.config.Config(
+        neat.DefaultGenome,
+        neat.DefaultReproduction,
+        neat.DefaultSpeciesSet,
+        neat.DefaultStagnation,
+        config_path
+    )
+
+    pop = neat.Population(config)
+    pop.run(eval_genomes, 50)
+
+
+if __name__ == '__main__':
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, 'config.txt')
+    run(config_path)
